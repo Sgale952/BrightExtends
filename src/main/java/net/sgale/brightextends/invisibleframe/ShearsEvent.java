3c9902cc59007a1cd74e7387b6a0f38a11be6194
Sgale <bread952@list.ru>
2023-08-17 15:41:36 +0700

    Delete useless data

---
+++ src/main/java/net/sgale/brightextends/invisibleframe/ShearsEvent.java
@@ -0,0 +1,11 @@
+package net.sgale.brightextends.invisibleframe;
+
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ShearsEvent {
+    @SubscribeEvent
+    public void onRightclickEntity(PlayerInteractEvent.EntityInteract event){
+
+    }
+}
